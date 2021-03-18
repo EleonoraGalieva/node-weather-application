06a524abdf50fd98403a04fa6080b53c5ba07fda
EleonoraGalieva <ela.galieva.2013@mail.ru>
2021-03-18 23:22:51 +0300

    Changed returning forecast info.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It\'s currently ' + body.current.temperature + ' degrees out, it feels like ' + body.current.feelslike + ' degrees.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It\'s currently ' + body.current.temperature + ' degrees out, it feels like ' + body.current.feelslike + ' degrees. The wind speed is ' + body.current.wind_speed + ' km per hour. The pressure is ' + body.current.pressure + ". The humidity is " + body.current.humidity + " %.")
         }
     })
 }
